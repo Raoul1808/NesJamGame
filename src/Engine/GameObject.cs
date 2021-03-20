3e28edf45a161313b085d57fdb0bf3f67f6042e3
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2021-03-20 12:04:16 +0100

    Added Game Object base

---
+++ src/Engine/GameObject.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NesJamGame.Engine.Graphics;
+using System.Collections.Generic;
+
+namespace NesJamGame.Engine
+{
+    public class GameObject
+    {
+        Dictionary<string, Animation> Animations;
+        string CurrentAnimation;
+        Vector2 position;
+
+        public GameObject(Vector2 position)
+        {
+            this.position = position;
+            Animations = new Dictionary<string, Animation>();
+        }
+
+        public void AddAnimation(Animation animation, string name)
+        {
+            Animations.Add(name, animation);
+            if (!Animations.ContainsKey(CurrentAnimation))
+            {
+                CurrentAnimation = name;
+            }
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            Animations[CurrentAnimation].Draw(spriteBatch, new Vector2((int)position.X, (int)position.Y));
+        }
+    }
+}
