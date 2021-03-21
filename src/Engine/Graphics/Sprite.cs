8042ef6684f8f6d99a3ff1e0a9b2e5a4d04612f5
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2021-03-21 10:17:31 +0100

    Very, VERY basic sprite for jam version

---
+++ src/Engine/Graphics/Sprite.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NesJamGame.Engine.Graphics
+{
+    public class Sprite
+    {
+        public Texture2D texture;
+        public Rectangle rectangle;
+        public Vector2 offset;
+        public float rotation;
+        public Color color;
+        public SpriteEffects flip;
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, null, color, rotation, offset, flip, 0f);
+        }
+
+        public void Draw(SpriteBatch spriteBatch, Point point)
+        {
+            spriteBatch.Draw(texture, new Rectangle(point.X, point.Y, rectangle.Width, rectangle.Height), null, color, rotation, offset, flip, 0f);
+        }
+    }
+}
