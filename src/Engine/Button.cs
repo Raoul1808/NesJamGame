527b5f45ac73e80bf9ddfbb2fb7d57e8233b67a4
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2021-03-21 12:37:00 +0100

    Updated Button to be much more simple

--- src/Engine/Button.cs
+++ src/Engine/Button.cs
@@ -1,22 +1,17 @@
 ﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using NesJamGame.Engine.Graphics;
 using NesJamGame.Engine.Input;
-using System.Collections.Generic;
 
 namespace NesJamGame.Engine
 {
     public abstract class Button
     {
-        public List<Sprite> sprites;
-        public Point position;
-        protected int currentSprite = 0;
+        public Rectangle rectangle;
         bool pressed;
 
         public virtual void Update()
         {
             Point mousePos = InputManager.GetMousePos();
-            if (sprites[currentSprite].rectangle.Contains(mousePos))
+            if (rectangle.Contains(mousePos))
             {
                 if (InputManager.IsNewPress(MouseButtons.Left))
                 {
@@ -40,7 +35,6 @@ namespace NesJamGame.Engine
             else if (InputManager.IsNewRelease(MouseButtons.Left) && pressed)
             {
                 pressed = false;
-                sprites[currentSprite].color = Color.White;
                 OnClickCancel();
             }
             else
@@ -49,39 +43,28 @@ namespace NesJamGame.Engine
             }
         }
 
-        public virtual void Draw(SpriteBatch spriteBatch)
-        {
-            sprites[currentSprite].Draw(spriteBatch, position);
-        }
-
         public virtual void OnIdle()
         {
-
         }
 
         public virtual void OnHover()
         {
-            sprites[currentSprite].color = Color.LightGray;
         }
 
         public virtual void OnStartClick()
         {
-            sprites[currentSprite].color = Color.Gray;
         }
 
         public virtual void OnClickHeld()
         {
-
         }
 
         public virtual void OnValidClick()
         {
-            
         }
 
         public virtual void OnClickCancel()
         {
-
         }
     }
 }
